b1c2f430509723e8499eefe0295a32014da7e13f
zuishuainanhai <1705902650@qq.ocm>
2021-09-18 11:21:54 +0800

    首页功能完成

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,2 +1,31 @@
 // 设置公共的基础路径 要引入在axios之后
-axios.defaults.baseURL='http://api-breakingnews-web.itheima.net'
+axios.defaults.baseURL = 'http://api-breakingnews-web.itheima.net'
+// 设置请求头
+// axios.defaults.headers.comon['Authorization'] = localStorage.getItem('token') || ''
+// 注册和登录不需要 拦截器
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 拦截所有的ajax的请求，成功后交给函数处理，config代表请求的相关配置
+    // 操作
+    console.log(config);
+    // 判断路径是否有/my/
+    if (config.url.indexOf('/my/') !== -1) {
+        config.headers.Authorization = localStorage.getItem('token') || ''
+    }
+    // 必须要返回
+    return config
+}, function (error) {
+    return Promise.reject(error)
+});
+// 响应拦截器 登录拦截
+axios.interceptors.response.use(function (response) {
+    // 响应信息
+    console.log(response.data);
+    if (response.data.message === '身份认证失败！') {
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+    }
+    return response
+}, function (error) {
+    return Promise.reject(error)
+})
---
+++ assets/js/index.js
@@ -0,0 +1,51 @@
+// 入口函数
+$(function () {
+    // 函数调用 渲染头像和用户名昵称
+    getUserInfo()
+    // 点击退出
+    $('#logout').on('click', function () {
+        layer.confirm('确认退出吗?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+// 封装函数渲染欢迎和头像为全局函数后面调用 局部可以调用全局函数
+function getUserInfo() {
+    axios({
+        // axios的属性是严格区分大小写
+        method: "GET",
+        url: '/my/userinfo',
+        // 身份认证 axios设置请求头在文档中  对象 
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // }
+    }).then(res => {
+        // console.log(res);
+        if (res.data.status !== 0) {
+            return layui.layer.msg(res.data.message)
+        }
+        // 成功渲染
+        renderAvatar(res.data.data)
+    })
+};
+
+function renderAvatar(user) {
+    // 欢迎
+    //获取用户的昵称或者用户名 逻辑运算
+    let name = user.nickname || user.username
+    $('.welcome').html('欢迎' + name)
+    // 头像 如果没有用户头像会返回null
+    if (user.user_pic !== null) {
+        // 隐藏文字头像，显示用户头像
+        $('.avatar-text').hide()
+        $('.layui-nav-img').show().attr('src', res.data.user_pic)
+    } else {
+        $('.layui-nav-img').hide()
+        let first = name[0].toUpperCase()
+        $('.avatar-text').show().html(first)
+
+    }
+}
